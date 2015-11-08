6f89c22dac08d8ca0fde4acd20aacac3d663422f
ElenaPoleschuk <Elena_poleschuk@epam.com>
2015-11-08 18:19:06 +0300

    Hometask4

--- Elena_Poleschuk/Module 3 -  Java/Week 2/Hometask4/src/by/epam/tat/lecture2/task1/utils/Communicator.java
+++ Elena_Poleschuk/Module 3 -  Java/Week 2/Hometask4/src/by/epam/tat/lecture2/task1/utils/Communicator.java
@@ -17,7 +17,6 @@ public class Communicator {
 	 }
 	
 	public static boolean getFlag(){
-	//	Scanner in = new Scanner ( System.in );
 		String answerContinue = in.next();
 		if (answerContinue.equalsIgnoreCase("y")){
 			return true;
@@ -27,7 +26,6 @@ public class Communicator {
 	}
 		
 	public static int intScanner(){
-	//	Scanner in = new Scanner(System.in);
 		int intValue = 0;
 		boolean a = true;
 		while (a){
@@ -44,7 +42,6 @@ public class Communicator {
 	}	
 	
 	public static String stringScanner(){
-	//	Scanner in = new Scanner(System.in);
 		String stringValue = in.nextLine();
 		return stringValue;
 	}	
